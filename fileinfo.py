878655d40d73320a45adaddd178a16d028c985c6
Fan <>
2020-10-11 16:40:01 +0800

    use __new__ to ignore invalid file

--- fileinfo.py
+++ fileinfo.py
@@ -1,15 +1,11 @@
-from pathlib import PurePath
+"""This is the example module.
 
-import hashlib
-import zlib
+This module does stuff.
+"""
 
-
-def valid(path, filename):
-    fullpath = path.joinpath(filename)
-    stat = fullpath.stat()
-    if fullpath.is_file() and stat.st_size > 0:
-        return True
-    return False
+from hashlib import sha256
+from pathlib import Path
+from zlib import adler32
 
 
 class FileInfo(object):
@@ -18,29 +14,45 @@ class FileInfo(object):
     """
     pass
 
-    def __init__(self, path, filename):
+    def __new__(cls, path: Path, filename: str):
         """
         docstring
         """
-        fullpath = path.joinpath(filename)
+        fullpath = path.joinpath(filename).resolve()
+        if not fullpath.is_file():
+            return None
 
-        self._digest = ''
-        self._hash = 0
-        self.path = fullpath.resolve()
-        self.stat = fullpath.stat()
+        stat = fullpath.stat()
+        if stat.st_size <= 0:
+            return UserWarning('Empty file: ' + str(fullpath))
+
+        checksum = None
+        try:
+            with fullpath.open(mode='rb') as f:
+                checksum = adler32(f.read(8192))
+        except OSError as e:
+            return e
+
+        instance = super().__new__(cls)
+        instance._hash = checksum
+        instance.path = fullpath
+        instance.stat = stat
+        return instance
+
+    def __init__(self, _path: Path, _filename: str):
+        """
+        docstring
+        """
+        self._digest = None
         self.uri = str(self.path)
         pass
 
     def __eq__(self, other):
-        return self.stat.st_size == other.stat.st_size and self.uri != other.uri and hash(self) > 0 and hash(self) == hash(other) and self.digest() == other.digest()
+        return self.stat.st_size == other.stat.st_size \
+            and self.uri != other.uri and hash(self) == hash(other) \
+            and self.digest() == other.digest()
 
     def __hash__(self):
-        if self._hash <= 0:
-            try:
-                with self.path.open(mode='rb') as f:
-                    self._hash = zlib.adler32(f.read(8192))
-            except:
-                pass
         return self._hash
 
     def __repr__(self):
@@ -50,6 +62,6 @@ class FileInfo(object):
         """
         docstring
         """
-        if self._digest == '':
-            self._digest = hashlib.sha256(self.path.read_bytes()).hexdigest()
+        if self._digest is None:
+            self._digest = sha256(self.path.read_bytes()).hexdigest()
         return self._digest
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import argparse
-import os
+"""This is the example module.
 
-from fileinfo import FileInfo, valid
+This module does stuff.
+"""
+import argparse
+import logging
+import os
+from fileinfo import FileInfo
 from pathlib import Path
 
 
@@ -9,27 +13,38 @@ def tidy(directory, collection):
     for root, _, files in os.walk(directory):
         path = Path(root)
         for filename in files:
-            if not valid(path, filename):
-                continue
             fi = FileInfo(path, filename)
-            collection.setdefault(fi, []).append(str(fi))
+            if isinstance(fi, FileInfo):
+                collection.setdefault(fi, []).append(str(fi))
+            else:
+                logging.debug(fi)
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='remove duplicate files', prog='rmdup')
-    parser.add_argument('directories', metavar='directory', type=str, nargs='+',
+    parser = argparse.ArgumentParser(description='remove duplicate files',
+                                     prog='rmdup')
+    parser.add_argument('-debug',
+                        action='store_true',
+                        default=False,
+                        help='debug')
+    parser.add_argument('directories',
+                        metavar='directory',
+                        type=str,
+                        nargs='+',
                         help='the directory to find duplicate')
 
     args = parser.parse_args()
+    # debug = parser.parse_args('-debug')
+    # if debug:
+    #     logging.basicConfig(level=logging.DEBUG)
     files = {}
     for directory in args.directories:
         tidy(directory, files)
 
-    for value in files.values():
-        if len(value) > 1:
-            print(value)
+    # for value in files.values():
+    #     if len(value) > 1:
+    #         print(value)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
